6054efb2b4d77e8040884ca2e615f0d761188fa8
Anugraha Covindarassou <anuradhiks26@gmail.com>
2026-07-01 14:38:45 +0530

    Add files via upload

---
+++ TCS practise codes/IntegerToBinary.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class IntegerToBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
+        String binary = Integer.toBinaryString(num);
+        System.out.println("Binary value: " + binary);
+        sc.close();
+    }
+}
